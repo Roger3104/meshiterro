f92575ebb6d2bd63451885a7eef3d56531e55f09
EC2 Default User <ec2-user@ip-172-31-8-55.ap-northeast-1.compute.internal>
2023-01-19 10:06:32 +0000

    [Finish] 5-11

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,28 @@
+class PostImagesController < ApplicationController
+  def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id 
+    # @post_image(投稿データ)のuser_idを、current_user.id(今ログインしているユーザーの ID)に
+    # 指定することで投稿データに、今ログイン中のユーザーの ID を持たせることができる。
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+  end
+  
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
+end
